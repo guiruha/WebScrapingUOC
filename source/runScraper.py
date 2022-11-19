fdd016c707f60d58c20ecb82e5356617c4b70306
Guillem <guillem.rochinaaguas@gmail.com>
2022-11-19 10:31:29 +0100

    Final preps + Memory + Commenting

--- source/runScraper.py
+++ source/runScraper.py
@@ -2,10 +2,10 @@ import os
 import sys
 import csv
 import pandas as pd
-from urlListingsSpider import UrlSpider
+from BookingScraper import BookingSpider
 
 if __name__ == "__main__":
-    booking = UrlSpider(checkin=sys.argv[1],
+    booking = BookingSpider(checkin=sys.argv[1],
                         checkout=sys.argv[2], 
                         city=sys.argv[3],
                         adults=sys.argv[4], 
@@ -13,17 +13,3 @@ if __name__ == "__main__":
                         rooms=sys.argv[6])
 
     booking.main()
-
-""" BORRAR   
-    #Concatenate the different csv files into a unique file
-    df_cols = ["city_searched", "check_in_searched", "check_out_searched", "room_config_searched", "name", "address",
-               "hotel_coordinates", "hotel_score", "hotel_scores", "hotel_description", "features", "room_data",
-               "count", "current_page", "in_page_count"]
-
-    df_all_cities = pd.DataFrame(columns = df_cols)
-    for file in os.listdir("cities_files/"):
-        df_city = pd.read_csv("cities_files/{}".format(file))
-        df_all_cities = pd.concat([df_all_cities, df_city])
-
-    df_all_cities.to_csv("cities_files/all_cities.csv")
-"""
